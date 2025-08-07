3363b5c28232c3d461c298cde55413206e906e7c
Fernando Aquino Terrazas <fernandoaquinoterrazas@MacBook-Air-de-Fernando.local>
2025-08-06 21:02:43 -0600

    nuevos componentes para la calculadora de inversiones

---
+++ src/app/user-income/user-income.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-income',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-income.component.html',
+  styleUrl: './user-income.component.css'
+})
+export class UserIncomeComponent {
+
+}
