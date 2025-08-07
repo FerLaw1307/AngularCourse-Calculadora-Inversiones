539379f0acdb0e87c106023ea3c114d69f4d94fa
Fernando Aquino Terrazas <fernandoaquinoterrazas@MacBook-Air-de-Fernando.local>
2025-08-06 21:45:05 -0600

    Data binding ajuste de calculo de inversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserIncomeComponent } from './user-income/user-income.component';
 @Component({
   selector: 'app-raiz',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, UserIncomeComponent]
 })
 export class AppComponent {}
--- src/app/user-income/user-income.component.ts
+++ src/app/user-income/user-income.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-income',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-income.component.html',
   styleUrl: './user-income.component.css'
 })
 export class UserIncomeComponent {
+  inversionInicialIngresada = '0';
+  inversionAnualIngresada = '0';
+  rendimientoEsperadoIngresado = '5';
+  duracionIngresada = '10';
 
+  onSend(){
+    console.log("Eviado!!!!!!!!!!!!");
+    console.log(this.inversionInicialIngresada);
+    console.log(this.inversionAnualIngresada);
+    console.log(this.rendimientoEsperadoIngresado);
+    console.log(this.duracionIngresada);
+  }
 }
